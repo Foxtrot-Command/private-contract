4f4d396fc982288b932ecd8867d71ce373f20bf1
Michydev <michyaraque@gmail.com>
2022-10-11 18:34:47 +0200

    feat: :building_construction: base configuration

---
+++ hardhat.config.ts
@@ -0,0 +1,61 @@
+import * as dotenv from "dotenv";
+import { extendEnvironment, HardhatUserConfig, task } from "hardhat/config";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+import "hardhat-contract-sizer";
+import "hardhat-docgen";
+
+dotenv.config();
+
+import "./tasks/tasks.ts";
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  abiExporter: {
+    path: "./build/abi",
+    clear: true,
+    flat: true,
+    spacing: 2,
+  },
+  docgen: {
+    path: "./docs",
+    clear: true,
+    runOnCompile: false,
+  },
+  networks: {
+    hardhat: {},
+    bsc: {
+      accounts: process.env.BSC_KEY !== undefined ? [process.env.BSC_KEY] : [],
+      url: `https://bsc-dataseed1.binance.org`,
+    },
+    bscTestnet: {
+      url: `https://data-seed-prebsc-1-s1.binance.org:8545`,
+      accounts:
+        process.env.BSC_TESNET_KEY !== undefined
+          ? [process.env.BSC_TESTNET_KEY]
+          : [],
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS === "true" ? true : false,
+    token: "BNB",
+    gasPriceApi: "https://api.bscscan.com/api?module=proxy&action=eth_gasPrice",
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.BSCSCAN_API_KEY,
+  },
+};
